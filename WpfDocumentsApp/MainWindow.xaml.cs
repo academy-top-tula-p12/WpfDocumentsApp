eec589c1da7730896b471fc7fbf3374d5e09ef4d
maxefimov <efimov.top.academy@gmail.com>
2024-04-04 20:45:54 +0300

    Добавьте файлы проекта.

---
+++ WpfDocumentsApp/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDocumentsApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            using(FileStream stream = new("document.xml", FileMode.Create))
+            {
+                XamlWriter.Save(docViewer.Document, stream);
+            }
+        }
+        private void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            using(FileStream stream = new("document.xml", FileMode.Open))
+            {
+                FlowDocument document = XamlReader.Load(stream) as FlowDocument;
+                docViewer.Document = document;
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            docViewer.ClearValue(FlowDocumentReader.DocumentProperty);
+        }
+
+
+    }
+}
